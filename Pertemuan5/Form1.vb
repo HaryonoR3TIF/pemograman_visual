3bf49fdce445376ff67c8ed6b4005482dcd1f2d6
HaryonoR3TIF <103356070+HaryonoR3TIF@users.noreply.github.com>
2023-04-13 20:02:15 +0530

    Add files via upload

--- Pertemuan5/Form1.vb
+++ Pertemuan5/Form1.vb
@@ -1,19 +1,14 @@
-﻿Public Class Form1
-    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-        If (Val(TxtMasaKerja.Text) >= 20) Then
-            TxtKeterangan.Text = "Anda mendapat bonus Rumah "
-
-        ElseIf (TxtMasaKerja.Text >= 15) Then
-            TxtKeterangan.Text = "Anda mendapat bonus Mobil"
-
-        ElseIf (TxtMasaKerja.Text >= 10) Then
-            TxtKeterangan.Text = "Anda mendapat bonus Motor"
-
-        ElseIf (TxtMasaKerja.Text >= 5) Then
-            TxtKeterangan.Text = "Anda mendapat bonus istri Sholehah"
-
+﻿
+Public Class Form1
+        Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+            If (Val(txtNilaiAkhir.Text) >= 60) Then
+            txtKeterangan2.Text = "Anda Lulus"
         Else
-            TxtKeterangan.Text = "Maaf anda belum layak dapat bonus"
+            txtKeterangan2.Text = "Anda tidak Lulus, Silahkan mengulang semester depan"
         End If
+        End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
