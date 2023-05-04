ae37bc7e341dd27899314ffca9e9b7ff5515030a
HaryonoR3TIF <103356070+HaryonoR3TIF@users.noreply.github.com>
2023-05-04 20:17:33 +0530

    Add files via upload

---
+++ Latihan/pertemuan 7/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Form1ToolstripMenuItem.Click
+        Dim F2 As New Form2
+        F2.ShowDialog()
+    End Sub
+
+    Private Sub Form2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Form2ToolStripMenuItem.Click
+        Dim F3 As New Form3
+        F3.ShowDialog()
+    End Sub
+End Class
